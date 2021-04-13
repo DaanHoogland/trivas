bb0f4026914c78a42b0280a02cef41e293e723e8
Daan Hoogland <dahn@onecht.net>
2021-04-13 14:41:49 +0200

    run in subdir

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 python2 -m pip install --user --upgrade setuptools pycodestyle pylint flask nose
 python2 -m pip install virtualenv
+cd subdir
+DIR=`pwd`
+cd
 sudo virtualenv -p `which python2` py2
 cd py2/
 source bin/activate
 which python
 which python2
 python --version
-cd
+cd $DIR
 bash -x runtests.sh
