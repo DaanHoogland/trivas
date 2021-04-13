cf1ed9ae871c87b7b4e77906aa1076b60a330aee
dahn <daan.hoogland@gmail.com>
2021-04-13 14:19:32 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+python2 -m pip install --user --upgrade setuptools pycodestyle pylint flask nose
+python2 -m pip install virtualenv
+sudo virtualenv -p `which python2` py2
+cd py2/
+source bin/activate
+which python
+which python2
+python --version
+bash -x runtests.sh
